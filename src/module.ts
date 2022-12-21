2eda9d77c3ff1da754081a1c46f6afcf2cc5e591
Pooya Parsa <pooya@pi0.io>
2022-12-21 16:17:34 +0100

    chore: format with prettier

--- src/module.ts
+++ src/module.ts
@@ -3,7 +3,11 @@ import { writeFile, mkdir } from 'node:fs/promises'
 import { dirname, resolve } from 'pathe'
 import { createDefu } from 'defu'
 import { defineNuxtModule, createResolver } from '@nuxt/kit'
-import { resolveSchema as resolveUntypedSchema, generateMarkdown, generateTypes } from 'untyped'
+import {
+  resolveSchema as resolveUntypedSchema,
+  generateMarkdown,
+  generateTypes,
+} from 'untyped'
 import type { Schema, SchemaDefinition } from 'untyped'
 // @ts-ignore
 import untypedPlugin from 'untyped/babel-plugin'
@@ -13,8 +17,12 @@ import jiti from 'jiti'
 export type NuxtConfigSchema = SchemaDefinition
 
 declare module '@nuxt/schema' {
-  interface NuxtConfig { ['$schema']?: NuxtConfigSchema }
-  interface NuxtOptions { ['$schema']: NuxtConfigSchema }
+  interface NuxtConfig {
+    ['$schema']?: NuxtConfigSchema
+  }
+  interface NuxtOptions {
+    ['$schema']: NuxtConfigSchema
+  }
   interface NuxtHooks {
     'schema:resolved': (schema: Schema) => void
     'schema:beforeWrite': (schema: Schema) => void
@@ -28,13 +36,15 @@ declare global {
 
 export default defineNuxtModule({
   meta: {
-    name: 'nuxt-config-schema'
+    name: 'nuxt-config-schema',
   },
-  async setup (options, nuxt) {
+  async setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
     // Initialize untyped/jiti loader
-    const virtualImports = await resolver.resolvePath('./runtime/virtual-imports')
+    const virtualImports = await resolver.resolvePath(
+      './runtime/virtual-imports'
+    )
     const _require = jiti(dirname(import.meta.url), {
       esmResolve: true,
       interopDefault: true,
@@ -42,21 +52,19 @@ export default defineNuxtModule({
       requireCache: false,
       alias: {
         '#imports': virtualImports,
-        'nuxt/config': virtualImports
+        'nuxt/config': virtualImports,
       },
       transformOptions: {
         babel: {
-          plugins: [
-            untypedPlugin
-          ]
-        }
-      }
+          plugins: [untypedPlugin],
+        },
+      },
     })
 
     // Create custom merger to dedup defaults in arrays
     const _defu = createDefu((obj, key, value) => {
       if (Array.isArray(obj[key]) && Array.isArray(value)) {
-        (obj as any)[key] = Array.from(new Set([...obj[key], ...value]))
+        ;(obj as any)[key] = Array.from(new Set([...obj[key], ...value]))
         return true
       }
     })
@@ -86,7 +94,7 @@ export default defineNuxtModule({
 
     // --- Bound utils ---
 
-    async function resolveSchema () {
+    async function resolveSchema() {
       // Global import
       // @ts-ignore
       globalThis.defineNuxtConfigSchema = (val: any) => val
@@ -94,16 +102,29 @@ export default defineNuxtModule({
       // Load schema from layers
       const schemas: Schema[] = []
       for (const layer of nuxt.options._layers) {
-        const filePath = await resolver.resolvePath(resolve(layer.config.rootDir, 'nuxt.schema'))
+        const filePath = await resolver.resolvePath(
+          resolve(layer.config.rootDir, 'nuxt.schema')
+        )
         if (filePath && existsSync(filePath)) {
           let loadedConfig
-          try { loadedConfig = _require(filePath) } catch (err) {
+          try {
+            loadedConfig = _require(filePath)
+          } catch (err) {
             // eslint-disable-next-line no-console
-            console.warn('[nuxt-config-schema] Unable to load schema from', filePath, err)
+            console.warn(
+              '[nuxt-config-schema] Unable to load schema from',
+              filePath,
+              err
+            )
             continue
           }
-          const lastLayerDefaults = schemas.length ? schemas[schemas.length - 1].default as any : {}
-          const schema = await resolveUntypedSchema(loadedConfig, lastLayerDefaults)
+          const lastLayerDefaults = schemas.length
+            ? (schemas[schemas.length - 1].default as any)
+            : {}
+          const schema = await resolveUntypedSchema(
+            loadedConfig,
+            lastLayerDefaults
+          )
           schemas.push(schema)
         }
       }
@@ -111,7 +132,10 @@ export default defineNuxtModule({
       // Merge config sources and resolve schema
       // @ts-expect-error
       const mergedSchema = _defu(...schemas) as Schema
-      const userSchema = await resolveUntypedSchema(nuxt.options.$schema, mergedSchema.default as any)
+      const userSchema = await resolveUntypedSchema(
+        nuxt.options.$schema,
+        mergedSchema.default as any
+      )
       const schema = _defu(userSchema, mergedSchema) as Schema
 
       // Allow hooking
@@ -120,18 +144,28 @@ export default defineNuxtModule({
       return schema
     }
 
-    async function writeSchema (schema: Schema) {
+    async function writeSchema(schema: Schema) {
       // Write it to build dir
       await mkdir(resolve(nuxt.options.buildDir, 'schema'), { recursive: true })
-      await writeFile(resolve(nuxt.options.buildDir, 'schema/nuxt.schema.json'), JSON.stringify(schema, null, 2), 'utf8')
+      await writeFile(
+        resolve(nuxt.options.buildDir, 'schema/nuxt.schema.json'),
+        JSON.stringify(schema, null, 2),
+        'utf8'
+      )
       const markdown = '# User config schema' + generateMarkdown(schema)
-      await writeFile(resolve(nuxt.options.buildDir, 'schema/nuxt.schema.md'), markdown, 'utf8')
+      await writeFile(
+        resolve(nuxt.options.buildDir, 'schema/nuxt.schema.md'),
+        markdown,
+        'utf8'
+      )
       const _types = generateTypes(schema, {
         addExport: true,
         interfaceName: 'NuxtUserConfig',
-        partial: true
+        partial: true,
       })
-      const types = _types + `
+      const types =
+        _types +
+        `
 export type UserAppConfig = Exclude<NuxtUserConfig['appConfig'], undefined>
 
 declare module '@nuxt/schema' {
@@ -140,8 +174,11 @@ declare module '@nuxt/schema' {
   interface AppConfigInput extends UserAppConfig {}
   interface AppConfig extends UserAppConfig {}
 }`
-      const typesPath = resolve(nuxt.options.buildDir, 'schema/nuxt.schema.d.ts')
+      const typesPath = resolve(
+        nuxt.options.buildDir,
+        'schema/nuxt.schema.d.ts'
+      )
       await writeFile(typesPath, types, 'utf8')
     }
-  }
+  },
 })
